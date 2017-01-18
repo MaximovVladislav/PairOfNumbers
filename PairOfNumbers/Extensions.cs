23902201e90b7b4e69c4545412b35485b3435fd1
MaximovVladislav <maximov.vladislav.90@gmail.com>
2017-01-18 23:12:31 +0300

    initialize

---
+++ PairOfNumbers/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairOfNumbers
+{
+    public static class Extensions
+    {
+        public static bool TryAdd<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            if (dictionary.ContainsKey(key))
+                return false;
+
+            dictionary.Add(key, value);
+            return true;
+        }
+    }
+}
---
+++ PairOfNumbers/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1000;
+
+            int startValue = 1;
+            int endValue = 10000;
+            int doubleEndValue = endValue * 2;
+
+            int[] numbers = new int[n];
+
+            // заполняем массив псевдослучайными значениями 
+            Random rand = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = rand.Next(startValue, endValue + 1);
+                //Console.Write("{0}, ", numbers[i]); // Долго выводит, надоело 
+            }
+
+            //numbers = new[] {8, 7, 4, 10, 6, 1, 7, 3}; 
+
+            Console.WriteLine();
+
+            Console.WriteLine("Введите число от {0} до {1}", startValue, doubleEndValue);
+            string str = Console.ReadLine();
+
+            int res;
+
+            if (int.TryParse(str, out res))
+            {
+                if (res <= doubleEndValue && res > startValue)
+                {
+                    DateTime starTime = DateTime.Now;
+                    var result = GetPairs(numbers, res);
+                    TimeSpan span = DateTime.Now - starTime;
+
+                    foreach (KeyValuePair<int, int> keyValuePair in result)
+                    {
+                        Console.WriteLine("{{{0}, {1}}}", keyValuePair.Value, keyValuePair.Key);
+                    }
+
+                    Console.WriteLine("Затрачено времени: {0}", span);
+                }
+                else
+                {
+                    Console.WriteLine("Число не входит в заданный диапазон");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не число");
+            }
+
+            Console.ReadKey();
+        }
+        static Dictionary<int, int> GetPairs(int[] numbers, int sum)
+        {
+            Dictionary<int, int> result = new Dictionary<int, int>();
+
+            Array.Sort(numbers);
+
+            int first = 0;
+            int last = numbers.Length - 1;
+
+            while (first < last)
+            {
+                int s = numbers[first] + numbers[last];
+
+                if (sum == s)
+                {
+                    result.TryAdd(numbers[first], numbers[last]);
+                    first++;
+                    last--;
+                }
+                else
+                {
+                    if (s < sum) first++;
+                    else last--;
+                }
+            }
+
+            return result;
+        }
+    }
+}
